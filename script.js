1dec84cc4aa89ac6b7bae5513259df871f6a3a22
letoproject <dmitry.sytnyk@gmail.com>
2024-02-11 04:21:57 +0200

    feat: edit script.js

--- script.js
+++ script.js
@@ -21,13 +21,16 @@ addNewBookBtn.addEventListener("click", (e) => {
   let pages = null;
 
   addBookToLibrary();
+  booksContainer.innerHTML = "";
   myLibrary.forEach((book) => {
+    const index = myLibrary.map((card) => card.title).indexOf(book.title);
     title = book.title;
     author = book.author;
     pages = book.pages;
+    read = book.read;
+    createBookCard(title, author, pages, read, index);
   });
 
-  addBookCard(title, author, pages);
   modalWindow.close();
   clearInput();
 });
@@ -36,48 +39,58 @@ function clearInput() {
   bookTitleInput.value = "";
   bookAuthorInput.value = "";
   bookPagesInput.value = "";
+  switchToggle.checked = false;
 }
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = false;
+  this.read = read;
 }
+
+Book.prototype.changeReadStatus = function (read) {
+  this.read = !this.read;
+};
 
 function addBookToLibrary() {
   title = bookTitleInput.value;
   author = bookAuthorInput.value;
   pages = bookPagesInput.value;
+  read = switchToggle.checked;
 
-  const newBook = new Book(title, author, pages);
-  newBook.read = switchToggle.addEventListener("click", (e) => readToggle(e));
+  // if (title === "" || author === "" || pages === "") {
+  //   alert("Inputs can`t be empty");
+  //   return;
+  // }
+
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
-function readToggle(e) {
-  return e.target.checked ? true : false;
-}
-
-function addBookCard(title, author, pages) {
+function createBookCard(title, author, pages, read, index) {
   const card = document.createElement("div");
+  card.setAttribute("data-id", `${index}`);
   card.classList.add("book-card");
 
   const bookTitle = document.createElement("p");
   const bookAuthor = document.createElement("p");
   const bookPages = document.createElement("p");
+  const haveRead = document.createElement("p");
 
   bookTitle.textContent = title;
   bookAuthor.textContent = author;
   bookPages.textContent = pages;
+  haveRead.textContent = read;
+
+  read ? (haveRead.style.color = "green") : (haveRead.style.color = "red");
 
   card.appendChild(bookTitle);
   card.appendChild(bookAuthor);
   card.appendChild(bookPages);
+  card.appendChild(haveRead);
   booksContainer.appendChild(card);
 }
-
-console.log(myLibrary);
 
 const currentYearSpan = document.querySelector(".current_year");
 currentYearSpan.textContent = new Date().getFullYear();
